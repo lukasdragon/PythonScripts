b5fe1d399290985c0bbe989060b655721c25561c
Lukas <lukasdragon@live.ca>
2017-11-01 11:50:54 -0700

    Add files via upload

---
+++ sentence counter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#===============================#
+#CODE PROPERTY OF LUKAS G. OLSON#
+#https://github.com/lukasdragon #
+#===============================#
+
+sentence = raw_input("Enter Sentence: ");
+sentence = str.split(str.strip(sentence));
+print ("You have " + str(len(sentence)) + " words in your sentence!");
