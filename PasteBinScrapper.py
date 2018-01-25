6ad8ff2f8faa1a6af6c44f352e059cc35a93c761
Lukas <lukasdragon@live.ca>
2018-01-25 17:58:00 +0000

    Work in progress to make multithreaded

--- PasteBinScrapper.py
+++ PasteBinScrapper.py
@@ -7,9 +7,22 @@
 
 #Imports
 from urllib2 import urlopen
+from ConfigParser import SafeConfigParser
 import json
 import os
 import time
+import sys
+from multiprocessing.dummy import Pool as ThreadPool
+
+
+
+
+######################################
+#DONT MODIFY ANYTHING PAST THIS POINT#
+###UNLESS YOU KNOW WHAT YOURE DOING###
+#ONLY MODIFY SETTINGS WITH CONFIG.INI#
+######################################
+
 
 class Severity():
     log = 0;
@@ -17,23 +30,13 @@ class Severity():
     warning = 2;
     failure = 3;
 
-
-#Settings
 class Settings:
-    url = "https://pastebin.com/api_scraping.php";
-    Key_Words = ["discord.gg", "minecraft", "leak",
-                 "password", "discord", "credit",
-                 "debit", "bank",
-                 "username", "leak", "code"];
-    filePath = "/files/"; #relative to script
-    idleTime = 40.0; #in seconds
+    url = "";
+    Key_Words = [];
+    filePath = "";
+    idleTime = 0;
     LogSeverity = Severity.log;
-
-
-######################################
-#DONT MODIFY ANYTHING PAST THIS POINT#
-###UNLESS YOU KNOW WHAT YOURE DOING###
-######################################
+    threads = 4;
 
 #Color Codes!
 COKGREEN = '\033[92m'; #0
@@ -44,19 +47,52 @@ CRESET ='\033[0m';
 CBOLD = '\033[01m';
 
 
-
 def log_message(severity,string):
     color = "";
+    prefix = "";
     if severity == Severity.log:
         color = COKGREEN;
+        prefix = "LOG: "
     elif severity == Severity.message:
         color = COKBLUE;
+        prefix = "MSG: "
     elif severity == Severity.warning:
         color = CBOLD + CWARNING;
     elif severity == Severity.failure:
         color = CBOLD + CFAIL;
     if severity >= Settings.LogSeverity:
-        print(color + string + CRESET);
+        print(color + prefix + string + CRESET);
+
+
+#read and write config file
+try:
+    config = SafeConfigParser();
+    config.read('config.ini');
+    Settings.url = config.get('main', 'api_url')
+    Settings.idleTime = config.getfloat('main', 'waitperiod')
+    Settings.filePath = config.get('main','filePath')
+    Settings.LogSeverity = config.getint('main','logLevel')
+    Settings.Key_Words = [e.strip() for e in config.get('main', 'keyWords').split(',')]
+    Settings.threads = config.getint('main', 'threads')
+except:
+    log_message(Severity.failure,"config.ini not found! Making one...")
+    config = SafeConfigParser()
+    config.read('config.ini')
+    config.add_section('main')
+    config.set('main', 'api_url', "https://pastebin.com/api_scraping.php")
+    config.set('main', 'filePath', '/files/')
+    config.set('main', 'waitperiod', '40.0')
+    config.set('main', 'threads', '4')
+    config.set('main', '; Minimum log severity', '')
+    config.set('main', '; 0 = log | 1 = message | 2 = warning | 3 = failure', '')
+    config.set('main', 'logLevel', '0')
+    config.set('main', '; The keywords required to download a file, seperated by commas', '')
+    config.set('main', 'keyWords', "discord.gg,bank,leak")
+    with open('config.ini', 'w') as f:
+        config.write(f)
+    log_message(Severity.failure, "Please edit config.ini and relaunch the script!")
+    sys.exit();
+
 
 #Gets the json file
 def get_jsonparsed_data(url):
@@ -76,6 +112,26 @@ def is_interesting(dictionary, content):
         if word in content.lower():
             return True;
 
+def scanData(page):
+    rescannedFiles = 0;
+    if not viewedFiles.__contains__(paste["key"]):
+        log_message(0,"Found a paste: " + paste["key"])
+        content = page.read();
+        viewedFiles.append(paste["key"]);
+
+    if is_interesting(Settings.Key_Words, content):
+        save_file(content, paste["key"])
+        log_message(1,"Found a match!: " + paste["key"]);
+        save_file(content, paste["key"])
+    else:
+        rescannedFiles += 1;
+        log_message(0, "Paste already scanned... Increasing wait time!")
+    return rescannedFiles;
+
+#Startup Image
+log_message(1,"========BY=======");
+log_message(1,"PasteBin Scrapper");
+log_message(1,"======Lukas======");
 
 #Creates the paste save folder if it doesn't exist
 Settings.filePath = (os.path.dirname(os.path.realpath(__file__)) + Settings.filePath);
@@ -85,42 +141,36 @@ except:
     pass;
 
 starttime=time.time()
-ticks = 0;
-rescannedFiles = 0;
+ticks, rescannedFiles = 0;
 viewedFiles = [];
 
-#Startup Image
-log_message(1,"========BY=======");
-log_message(1,"PasteBin Scrapper");
-log_message(1,"======Lukas======");
-
+#starts threading stuff
+pool = ThreadPool()
+pool = ThreadPool(Settings.threads)
 
 
 while True:
-    #Gets the pastebin recent pastes
-    data = get_jsonparsed_data(Settings.url);
+    try:
+        data = get_jsonparsed_data(Settings.url);
 
-    #Loops through each entry
+        #Loops through each entry
+        page = [];
+        for paste in data:
+            page.append(urlopen(paste["scrape_url"]));
 
-    for paste in data:
-        #Opens the individual pastes so that we can extract key words
-        page = urlopen(paste["scrape_url"]);
-        if not viewedFiles.__contains__(paste["key"]):
-            log_message(0,"Found a paste: " + paste["key"])
-            content = page.read();
-            viewedFiles.append(paste["key"]);
+        results = pool.map(scanData, page);
+        pool.close()
+        pool.join()
+        page = [];
 
-        #Goes through the page and checks for keywords
-            if is_interesting(Settings.Key_Words, content):
-                save_file(content, paste["key"])
-                log_message(1,"Found a match!: " + paste["key"]);
-                save_file(content, paste["key"])
-        else:
-            rescannedFiles += 1;
-            log_message(0, "Paste already scanned... Increasing wait time!")
-    ticks+=1;
-    idletime = ((rescannedFiles * 2) / ticks );
-    log_message(1, "Finished Tick!, waiting " + str(Settings.idleTime + idletime) + " seconds!");
 
+        for x in results:
+            rescannedFiles += x;
+
+        ticks+=1;
+        idletime = ((rescannedFiles * 2) / ticks );
+        log_message(1, "Finished Tick!, waiting " + str(Settings.idleTime + idletime) + " seconds!");
+    except:
+        pass;
 
     time.sleep((Settings.idleTime +idletime) - ((time.time() - starttime) % (Settings.idleTime + idletime)));
